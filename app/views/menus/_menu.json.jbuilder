75b37c2183238c79fdea2544da4c42571134e7ce
Marcus Soares <marcus.devfs@gmail.com>
2025-10-20 19:49:08 -0300

    feat: creates menu model and adds its MVC setup

---
+++ app/views/menus/_menu.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! menu, :id, :name, :created_at, :updated_at
+json.name menu.name
---
+++ spec/factories/menus.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :sushi_menu do
+    name { "Sushi Menu" }
+  end
+
+  factory :dinner_menu do
+    name { "Dinner Menu" }
+  end
+
+  factory :lunch_menu do
+    name { "Lunch Menu" }
+  end
+end
