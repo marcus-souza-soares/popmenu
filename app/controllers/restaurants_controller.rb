01daf29ccb98b828ed30209956c22d21edae3dc2
Marcus Soares <marcus.devfs@gmail.com>
2025-10-22 20:36:57 -0300

    perf: bullet gems and n+1 queries fixes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,7 @@ class RestaurantsController < ApplicationController
 
   # GET /restaurants or /restaurants.json
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant.all.includes(:menus)
   end
 
   # GET /restaurants/1 or /restaurants/1.json
